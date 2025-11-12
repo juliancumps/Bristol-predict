6d0bd683256261f2e44a60807bb606c5c802b911
mtech <jwtcumps@gmail.com>
2025-11-12 13:54:37 -0800

    syntax fix

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ function App() {
           {currentView === "efficiency" && (
             <CatchEfficiencyScreen
               onNavigateToRunTracker={() => setCurrentView("runtracker")}
+              onNavigateToWeather={() => setCurrentView("weather")}
               onBack={() => setCurrentView("map")}
             />
           )}
