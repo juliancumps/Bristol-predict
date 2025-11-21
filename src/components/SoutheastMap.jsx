b71428d7567b4a60a93adb4f6aa7990fe4b3c350
mtech <jwtcumps@gmail.com>
2025-11-21 14:08:54 -0800

    salmon sankey button since embed does is not supported for such tableau dash

--- src/components/SoutheastMap.jsx
+++ src/components/SoutheastMap.jsx
@@ -142,100 +142,130 @@ useEffect(() => {
           )}
 
           {activeTab === 'southeast' && (
-            <div className="tab-content southeast-content">
-              <div className="content-split-vertical">
-                {/* Map Container - ArcGIS Embed */}
-                <div className="split-item map-item">
-                  <iframe 
-                    src="https://arcg.is/1Knnfu0"
-                    className="southeast-map-alt"
-                    style={{ border: 'none' }}
-                    title="ArcGIS Map Viewer"
-                    allowFullScreen
-                  />
+          <div className="tab-content southeast-content">
+            <div className="content-split-vertical">
+              {/* Map Container - ArcGIS Embed */}
+              <div className="split-item map-item">
+                <iframe 
+                  src="https://arcg.is/1Knnfu0"
+                  className="southeast-map-alt"
+                  style={{ 
+                    border: 'none',
+                    width: '100%',
+                    height: '100%',
+                    display: 'block'
+                  }}
+                  title="ArcGIS Map Viewer"
+                  allowFullScreen
+                />
+              </div>
+
+              {/* Right Side - Dashboards Stacked */}
+              <div className="dashboards-column">
+                {/* First Dashboard Container */}
+                <div className="split-item tableau-item" style={{height: '550px'}}> 
+                  <div className='tableauPlaceholder' id='viz1763667574911' style={{ position: 'relative', width: '100%', height: '100%' }}>
+                    <noscript>
+                      <a href='#'>
+                        <img alt='Number of Salmon Caught by Fishery (SEAK)' src='https://public.tableau.com/static/images/SE/SEAKSalmonbyarea/SEAKSalmonbyArea/1_rss.png' style={{ border: 'none' }} />
+                      </a>
+                    </noscript>
+                    <object className='tableauViz' style={{ display: 'none' }}>
+                      <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+                      <param name='embed_code_version' value='3' />
+                      <param name='site_root' value='' />
+                      <param name='name' value='SEAKSalmonbyarea&#47;SEAKSalmonbyArea' />
+                      <param name='tabs' value='no' />
+                      <param name='toolbar' value='yes' />
+                      <param name='static_image' value='https://public.tableau.com/static/images/SE/SEAKSalmonbyarea/SEAKSalmonbyArea/1.png' />
+                      <param name='animate_transition' value='yes' />
+                      <param name='display_static_image' value='yes' />
+                      <param name='display_spinner' value='yes' />
+                      <param name='display_overlay' value='yes' />
+                      <param name='display_count' value='yes' />
+                      <param name='language' value='en-US' />
+                      <param name='filter' value='publish=yes' />
+                    </object>
+                  </div>
+                  <script type='text/javascript'>
+                    {`
+                      var divElement = document.getElementById('viz1763667574911');
+                      var vizElement = divElement.getElementsByTagName('object')[0];
+                      vizElement.style.width='100%';
+                      vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+                      var scriptElement = document.createElement('script');
+                      scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+                      vizElement.parentNode.insertBefore(scriptElement, vizElement);
+                    `}
+                  </script>
                 </div>
 
-                {/* Right Side - Dashboards Stacked */}
-                <div className="dashboards-column">
-                  {/* First Dashboard Container */}
-                  <div className="split-item tableau-item" style={{height: '550px'}}> 
-                    <div className='tableauPlaceholder' id='viz1763667574911' style={{ position: 'relative', width: '100%', height: '100%' }}>
-                      <noscript>
-                        <a href='#'>
-                          <img alt='Number of Salmon Caught by Fishery (SEAK)' src='https://public.tableau.com/static/images/SE/SEAKSalmonbyarea/SEAKSalmonbyArea/1_rss.png' style={{ border: 'none' }} />
-                        </a>
-                        </noscript>
-                      <object className='tableauViz' style={{ display: 'none' }}>
-                        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-                        <param name='embed_code_version' value='3' />
-                        <param name='site_root' value='' />
-                        <param name='name' value='SEAKSalmonbyarea&#47;SEAKSalmonbyArea' />
-                        <param name='tabs' value='no' />
-                        <param name='toolbar' value='yes' />
-                        <param name='static_image' value='https://public.tableau.com/static/images/SE/SEAKSalmonbyarea/SEAKSalmonbyArea/1.png' />
-                        <param name='animate_transition' value='yes' />
-                        <param name='display_static_image' value='yes' />
-                        <param name='display_spinner' value='yes' />
-                        <param name='display_overlay' value='yes' />
-                        <param name='display_count' value='yes' />
-                        <param name='language' value='en-US' />
-                        <param name='filter' value='publish=yes' />
-                      </object>
-                    </div>
-                    <script type='text/javascript'>
-                      {`
-                        var divElement = document.getElementById('viz1763667574911');
-                        var vizElement = divElement.getElementsByTagName('object')[0];
-                        vizElement.style.width='100%';
-                        vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
-                        var scriptElement = document.createElement('script');
-                        scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-                        vizElement.parentNode.insertBefore(scriptElement, vizElement);
-                      `}
-                    </script>
+                {/* Second Dashboard Container */}
+                <div className="split-item tableau-item" style={{height: '500px'}}>
+                  <div className='tableauPlaceholder' id='viz1763667608060' style={{ position: 'relative', width: '100%', height: '100%' }}>
+                    <noscript>
+                      <a href='#'>
+                        <img alt='Average Price ($) of Sockeye per Fishery Area (SEAK)' src='https://public.tableau.com/static/images/SE/SEAKSalmonbyarea/SEAKSalmonPriceWeightbyArea/1_rss.png' style={{ border: 'none' }} />
+                      </a>
+                    </noscript>
+                    <object className='tableauViz' style={{ display: 'none' }}>
+                      <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+                      <param name='embed_code_version' value='3' />
+                      <param name='site_root' value='' />
+                      <param name='name' value='SEAKSalmonbyarea&#47;SEAKSalmonPriceWeightbyArea' />
+                      <param name='tabs' value='no' />
+                      <param name='toolbar' value='yes' />
+                      <param name='static_image' value='https://public.tableau.com/static/images/SE/SEAKSalmonbyarea/SEAKSalmonPriceWeightbyArea/1.png' />
+                      <param name='animate_transition' value='yes' />
+                      <param name='display_static_image' value='yes' />
+                      <param name='display_spinner' value='yes' />
+                      <param name='display_overlay' value='yes' />
+                      <param name='display_count' value='yes' />
+                      <param name='language' value='en-US' />
+                      <param name='filter' value='publish=yes' />
+                    </object>
                   </div>
+                  <script type='text/javascript'>
+                    {`
+                      var divElement = document.getElementById('viz1763667608060');
+                      var vizElement = divElement.getElementsByTagName('object')[0];
+                      izElement.style.width='100%';
+                      vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+                      var scriptElement = document.createElement('script');
+                      scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+                      vizElement.parentNode.insertBefore(scriptElement, vizElement);
+                    `}
+                  </script>
+                </div>
 
-                  {/* Second Dashboard Container */}
-                  <div className="split-item tableau-item" style={{height: '500px'}}>
-                    <div className='tableauPlaceholder' id='viz1763667608060' style={{ position: 'relative', width: '100%', height: '100%' }}>
-                      <noscript>
-                        <a href='#'>
-                          <img alt='Average Price ($) of Sockeye per Fishery Area (SEAK)' src='https://public.tableau.com/static/images/SE/SEAKSalmonbyarea/SEAKSalmonPriceWeightbyArea/1_rss.png' style={{ border: 'none' }} />
-                        </a>
-                      </noscript>
-                      <object className='tableauViz' style={{ display: 'none' }}>
-                        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-                        <param name='embed_code_version' value='3' />
-                        <param name='site_root' value='' />
-                        <param name='name' value='SEAKSalmonbyarea&#47;SEAKSalmonPriceWeightbyArea' />
-                        <param name='tabs' value='no' />
-                        <param name='toolbar' value='yes' />
-                        <param name='static_image' value='https://public.tableau.com/static/images/SE/SEAKSalmonbyarea/SEAKSalmonPriceWeightbyArea/1.png' />
-                        <param name='animate_transition' value='yes' />
-                        <param name='display_static_image' value='yes' />
-                        <param name='display_spinner' value='yes' />
-                        <param name='display_overlay' value='yes' />
-                        <param name='display_count' value='yes' />
-                        <param name='language' value='en-US' />
-                        <param name='filter' value='publish=yes' />
-                      </object>
-                    </div>
-                    <script type='text/javascript'>
-                      {`
-                        var divElement = document.getElementById('viz1763667608060');
-                        var vizElement = divElement.getElementsByTagName('object')[0];
-                        vizElement.style.width='100%';
-                        vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
-                        var scriptElement = document.createElement('script');
-                        scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-                        vizElement.parentNode.insertBefore(scriptElement, vizElement);
-                      `}
-                    </script>
-                  </div>
+                {/* Third Dashboard Container - Sankey Link Button */}
+                <div className="split-item tableau-item" style={{width: '50%', height: '80px', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#f5f5f5'}}>
+                  <a 
+                    href="https://public.tableau.com/views/SEAKSalmonSankey/SEAKSalmonGearSankey?:language=en-US&:sid=&:redirect=auth&:toolbar=no&:display_count=no&:showVizHome=no&:tabs=no&:origin=viz_share_link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                      display: 'inline-block',
+                      padding: '16px 32px',
+                      backgroundImage: 'linear-gradient(155deg, #1f77b4 0%, #15aa9e 100%)',
+                      color: 'white',
+                      textDecoration: 'none',
+                      borderRadius: '15px',
+                      fontSize: '16px',
+                      fontWeight: 'bold',
+                      cursor: 'pointer',
+                      transition: 'background-color 0.3s',
+                    }}
+                    onMouseOver={(e) => e.target.style.backgroundImage = 'linear-gradient(155deg, #15aa9e 0%, #1f77b4 100%)'}
+                    onMouseOut={(e) => e.target.style.backgroundImage = 'linear-gradient(155deg, #1f77b4 0%, #15aa9e 100%)'}
+                  >
+                    View Salmon Caught by Gear Type →
+                  </a>
                 </div>
               </div>
             </div>
-          )}
+          </div>
+        )}
         </div>
       </div>
     </div>
